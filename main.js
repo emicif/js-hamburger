a63c757f850c2d40aa1216078a13289434cd5bda
emicif <emanuela.cifolelli@gmail.com>
2022-03-07 16:12:37 +0100

    menu

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 console.log('JS Ok');
+
+
+// menu display block
+const hamburgerMenu = document.querySelector ('.hamburger-menu');
+
+
+
+
+const headerRight = document.querySelector ('i');
+
+
+function  () {
+    if (hamburgerMenu.style.display === "block") {
+        hamburgerMenu.style.display = "none";
+    } else {
+        hamburgerMenu.style.display = "block";
+    }
+}
