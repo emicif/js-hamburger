3d7958dc300b9d37beff555b664770964004acb5
emicif <emanuela.cifolelli@gmail.com>
2022-03-08 10:00:59 +0100

    end

--- main.js
+++ main.js
@@ -1,21 +1,44 @@
-console.log('JS Ok');
+console.log('JS OK!');
 
 
-// menu display block
+// menu display block - a figli diretti di header-right
+const hamburgerIcon = document.querySelector('.header-right > a');
+
+// entro nel close
+const closeIcon = document.querySelector('.close');
+
+// entro nell'hamburger menu
 const hamburgerMenu = document.querySelector ('.hamburger-menu');
 
 
-
-
-const headerRight = document.querySelector ('i');
-
-
-
-headerRight.addEventListener('click', function{
-    if (hamburgerMenu.style.display === "block") {
-    } else {
-        hamburgerMenu.style.display = "none";
+// aggiungo l'evento - commenti ci sono le opzioni possibili
+hamburgerIcon.addEventListener('click',
+    function () {
+      // hamburgerMenu.style.display = 'block';
+      const classesInElement = hamburgerMenu.className;
+      hamburgerMenu.className = classesInElement + ' active';
+      // hamburgerMenu.className = hamburgerMenu.className + ' active'; // alternativa
+      // hamburgerMenu.className += ' active'; // alternativa
+      //
     }
-})
+)
 
 
+/* Alternativa consigliata per aggiungere l'evento 
+    
+    hamburgerIcon.addEventListener('click',
+    function () {
+        hamburgerMenu.classList.add('active');
+        
+        }
+) */
+
+
+
+// Close icone
+
+closeIcon.addEventListener('click',
+    function() {
+        hamburgerMenu.classList.remove('active');
+    }
+)
